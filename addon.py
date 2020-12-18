6566952bc7d8f82f544818b03c7b3570f129c76d
Davide Bisso <33525392+DaxterXS@users.noreply.github.com>
2020-12-18 21:58:41 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -6,15 +6,15 @@ url = 'https://apid.sky.it/vdp/v1/getLivestream?id=2&jsonp=jsonP.queue.jsonP0_15
 res = requests.get(url)
 
 try:
-	streaming_url = 'https://' + re.findall('"streaming_url":"https{0,1}://(.*?)"', res.text)[0]
+	streaming_url = 'https://sbshdlu5-lh.akamaihd.net/i/sbshdl_1@810993/master.m3u8?hdnts=st=1583957164~exp=1584043564~acl=/i/*~hmac=1e353be7417c95a775f53b60bbfce7da92317c917c0c6d42e36a7ad282adef96&mux_audio=true'
 	thumb_url = 'https://' + re.findall('"img":"https{0,1}://(.*?)"', res.text)[1]
-	listitem = xbmcgui.ListItem('Cielo TV')
-	listitem.setInfo('video', {'Title': 'Cielo TV'})
+	listitem = xbmcgui.ListItem('MotorTrend')
+	listitem.setInfo('video', {'Title': 'MotorTrend'})
 	listitem.setArt({ 'thumb': thumb_url})
 	xbmc.Player().play(streaming_url, listitem)
 
 except Exception as ex:
 	message = 'Eccezione: %s' % ex
-	listitem = xbmcgui.ListItem('Cielo - Errore')
-	listitem.setInfo('video', {'Title': 'Errore nell\' avvio del plugin Cielo'})
+	listitem = xbmcgui.ListItem('MotorTrend - Errore')
+	listitem.setInfo('video', {'Title': 'Errore nell\' avvio del plugin MotorTrend'})
 	listitem.setInfo('video', { 'plot': message })
